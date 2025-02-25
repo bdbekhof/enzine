a176b43eb09abd7755b357efe9f2a7f086e7f831
Brian <124669631+bdbekhof@users.noreply.github.com>
2025-02-25 14:45:04 +0100

    Discovered bug in feutenawards, storing to fix

--- src/pages/Redactie.tsx
+++ src/pages/Redactie.tsx
@@ -6,7 +6,7 @@ const Redactie = () => {
     });
 
     return (
-        <div className="homeText">
+        <div className="redactie">
             <h1>Hét karakterloze Redactie der ['endzjin] S.V.E.I.A.</h1>
             <p>Lux et Libertas ofwel “Licht en Vrijheid”. O.a. motto van de universiteit van Yale en het NRC handelsblad. Wij delen met geen van beide een band, enkel de ideologie uit het liberalisme.</p>
         </div>
