74236e408fdb43cc4796026da848bf90ee0dd48f
Brian <124669631+bdbekhof@users.noreply.github.com>
2025-02-25 12:15:56 +0100

    Styled homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import styles from '../styles/home.module.css';
 
 const Home = () => {
     useEffect(() => {
@@ -6,7 +7,7 @@ const Home = () => {
     });
 
     return (
-        <div className="homeText">
+        <div className={styles.homeText}>
             <h1>Hét karakterloze studentenmagazine der ['endzjin] S.V.E.I.A.</h1>
             <p>Lux et Libertas ofwel “Licht en Vrijheid”. O.a. motto van de universiteit van Yale en het NRC handelsblad. Wij delen met geen van beide een band, enkel de ideologie uit het liberalisme.</p>
         </div>
