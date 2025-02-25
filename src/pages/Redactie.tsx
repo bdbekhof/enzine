958928cba3670b326696cf099782f4d4ff144e7f
Brian <124669631+bdbekhof@users.noreply.github.com>
2025-02-25 15:35:23 +0100

    Added redactie text

--- src/pages/Redactie.tsx
+++ src/pages/Redactie.tsx
@@ -7,8 +7,23 @@ const Redactie = () => {
 
     return (
         <div className="redactie">
-            <h1>Hét karakterloze Redactie der ['endzjin] S.V.E.I.A.</h1>
-            <p>Lux et Libertas ofwel “Licht en Vrijheid”. O.a. motto van de universiteit van Yale en het NRC handelsblad. Wij delen met geen van beide een band, enkel de ideologie uit het liberalisme.</p>
+            <h1>De redactie</h1>
+            <p>
+                De eeuwenoude vragen die de leden al eeuwenlang bezighoudt. Wat gebeurt er eigenlijk allemaal binnen ['endzjin]? Wie doet wat? Wie niet? Wat gebeurt er achter gesloten deuren? 
+                Wie heeft wie gezien, en met wie heeft wie iets gedaan? Wat is de stand van zaken met de disputen? 
+                En, het belangrijkste: welke feut wint dit jaar een award? Allemaal brandende vragen die wachten tot beantwoording in de ['en-z!ne]! Maar, hé, wacht even. Wie schrijft deze onmisbare antwoorden? 
+                Uiteraard wij – de redactie van de ['en-z!ne]. En geloof ons, we zijn niet zomaar een stelletje studenten die wat woorden aan elkaar rijgen. Nee, wij zijn de crème de la crème van het universitaire journaille. 
+                Wij weten wel hoe we de ['en-z!ne] verwoorden, dan klinkt het allemaal veel beter dan zo'n simpel roddeltje van een lid.
+                Vergis je niet, het bladeren door de ['en-z!ne] lijkt misschien een simpel appeltje-eitje, een beetje lachen, gieren, brullen. Maar wij weten wel beter. 
+                Het kost bloed, zweet, tranen, en een paar liters alcohol om deze uitgaves de wereld in te sturen. 
+                En natuurlijk, de nodige creatieve discussies over wie de beste juice heeft. (Spoiler: die hebben wij.) 
+                De redactie is van tevoren al maanden bezig met het uitpluizen van de sappigste roddels, het ophalen van de heetste nieuwtjes, en het schaduwen van de disputen. 
+                Wij zien alles. Het lijkt wel magie, maar het is gewoon werk. 
+                En dan, het hoogtepunt waar ieder lid altijd bij wil zijn om de roddels te weten: het ['en-z!ne] weekend. 
+                Waar de rest zich beperkt tot het genieten van ons meesterwerk, werken wij als machine achter de schermen om de ultieme uitgaves neer te zetten. 
+                Denk jij na het lezen van dit stuk tekst, ik heb dit ook in me? Laat dan een berichtje achter bij de hoofdredactie.
+            </p>
+            <p>Zonder redactie is er geen ['en-z!ne] en zonder ['en-z!ne] ben jij niet op de hoogte!</p>
         </div>
     );
 };
